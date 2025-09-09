0a45b9cf0f93c881c8054eee21d3ed5bc403203e
jhonnybrzz1 <jose.jonathan@hotmail.com>
2025-09-09 04:01:04 -0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
   try {
     const existingKey = await storage.getActiveApiKey();
     if (!existingKey) {
-      await storage.createApiKey({ mistralKey: "Ahd97U350sYNv1F0uenTheZjrIf5gWQR" });
+      await storage.createApiKey({ mistralKey: });
       log("Mistral API key initialized");
     }
   } catch (error) {
